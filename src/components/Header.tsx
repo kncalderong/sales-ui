d1ab6a9a0079dc61d08c3c0b1cebb4884dc54ff3
Kevin Nicolas <kncalderong@gmail.com>
2023-07-21 12:47:47 -0500

    feat: login information content of landing page completed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,22 +5,22 @@ import { useState } from 'react'
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
   return (
-    <header>
-      <div className='w-full px-4 py-6 bg-slate-100 shadow-md relative flex justify-end items-center'>
+    <header className='relative w-full'>
+      <div className='w-full px-4 py-6 bg-slate-100 shadow-md relative flex justify-end items-center md:hidden'>
         <FontAwesomeIcon
           icon={faBars}
           className={`text-2xl cursor-pointer transition-all duration-300 ${
             isMenuOpen ? 'rotate-90' : 'rotate-0'
-          }`}
+          } `}
           onClick={() => setIsMenuOpen(!isMenuOpen)}
         />
       </div>
       <div
         className={`w-full flex flex-col gap-4 bg-slate-100 overflow-hidden transition-all duration-300  ${
           isMenuOpen ? 'h-[140px] px-6 py-4' : 'h-0 p-0'
-        }`}
+        } md:h-auto md:flex-row-reverse md:bg-transparent md:justify-start md:px-14 md:py-4 md:text-2xl md:absolute md:top-10 md:left-0 md:gap-10 md:z-20`}
       >
-        <a href='#login-section' className='text-blue-500 font-extrabold'>
+        <a href='#login-section' className='text-primaryBlue font-extrabold'>
           Login
         </a>
         <a href='#content-1'>Content 1</a>
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -6,12 +6,26 @@ import { useAppContext } from '../context/appContext'; */
 const Landing = () => {
   //const { user } = useAppContext();
   return (
-    <main
-      className={`bg-[url("/bg-landing.png")] bg-cover bg-[-360px] lg:bg-[0px]  min-h-screen w-full`}
-    >
+    <main className={`min-h-screen w-full`}>
       {/* {user && <Navigate to='/' />} */}
       <Header />
-      <div id='login-section' className='min-h-screen'></div>
+      <div
+        id='login-section'
+        className=' relative min-h-screen bg-[url("/bg-landing.png")] bg-no-repeat bg-[-360px_-10px] md:bg-[-75%_-10px] xl:bg-right'
+      >
+        <div className='flex flex-col gap-3 absolute top-1/4 w-[80%] left-[10%] translate-x-0 -translate-y-1/2 md:w-[550px] md:gap-6 md:bg-white md:rounded-[1.5rem] md:top-[50%] md:p-8 lg:left-[15%]'>
+          <h1 className='text-slate-700 text-3xl md:text-[4rem] md:leading-[4rem] md:font-extrabold'>
+            Lorem ipsum Design
+          </h1>
+          <p className='text-slate-400 md:text-lg'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua
+          </p>
+          <div className='cursor-pointer text-white bg-primaryBlue flex justify-center items-center px-8 py-4 w-[140px] mt-12 md:text-2xl'>
+            Login
+          </div>
+        </div>
+      </div>
       <div id='content-1' className='min-h-screen'></div>
       <div id='content-2' className='min-h-screen'></div>
     </main>

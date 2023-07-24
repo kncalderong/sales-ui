1b2300b17337143791ae07f5e8be9701c8c5fb08
Kevin Nicolas <kncalderong@gmail.com>
2023-07-23 19:20:26 -0500

    feat: logout feature included on profile page

--- src/pages/dashboard/Profile.tsx
+++ src/pages/dashboard/Profile.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import { useAppContext } from '../../context/appContext'
 
 const Profile = () => {
-  return <div>Profile</div>
+  const { logoutUser } = useAppContext()
+
+  return (
+    <section className='w-full bg-slate-100 flex justify-center items-start pt-[20%] lg:py-[5.5rem]'>
+      <div className='w-full max-w-[80%] lg:max-w-[880px] 2xl:max-w-[1024px]'>
+        <div
+          className='py-4 px-8 bg-primaryBlue w-[40%] flex justify-center items-center text-white cursor-pointer lg:w-[180px]'
+          onClick={() => {
+            logoutUser()
+          }}
+        >
+          Logout
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default Profile
